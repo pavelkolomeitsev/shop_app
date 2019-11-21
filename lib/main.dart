276170a10d5c088fd94190d3238cb2a523170e62
Pavel Kolomeitsev <pavelkolomeitsev1981@gmail.com>
2019-11-21 13:50:24 +0200

    add authentication page

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shop_app/providers/cart.dart';
 
-import './pages/products_overview_page.dart';
 import './pages/product_detail_page.dart';
 import './providers/products_provider.dart';
 import './pages/cart_page.dart';
@@ -10,6 +9,7 @@ import './providers/orders.dart';
 import './pages/orders_page.dart';
 import './pages/user_products_page.dart';
 import './pages/edit_product_page.dart';
+import './pages/auth_page.dart';
 
 void main() => runApp(MyApp());
 
@@ -35,7 +35,7 @@ class MyApp extends StatelessWidget {
           accentColor: Colors.deepOrange,
           fontFamily: 'Lato',
         ),
-        home: ProductsOverviewPage(),
+        home: AuthPage(),
         routes: {
           ProductDetailPage.routeName: (context) => ProductDetailPage(),
           CartPage.routeName: (context) => CartPage(),
